ec683388ebb6de1cc4ef3ccd10b02e47c0750a67
Chasky91 <david_c91@outlook.com>
2025-09-30 19:57:05 -0300

    30/09

--- appterminal.js
+++ appterminal.js
@@ -1,6 +1,6 @@
 import * as readline from 'node:readline/promises'
 import { stdin as entrada, stdout as salida } from 'node:process'
-import { comentarios, VerComentarios, BuscarComentario } from './comentarios.js'
+import { eliminarComentario, VerComentarios, BuscarComentario } from './comentarios.js'
 
 const rl = readline.createInterface({
      input: entrada, 
@@ -12,7 +12,15 @@ const rl = readline.createInterface({
 const respuesta = await rl.question(
     'Buscar comentario: \n '
 )
+
+//estructura  que maneja   la gestion  o llamada de funciones
+if()
+
 BuscarComentario(Number(respuesta))
+VerComentarios()
+
+eliminarComentario()
 
 
- rl.close()
+
+rl.close()
--- comentarios.js
+++ comentarios.js
@@ -6,21 +6,30 @@ const comentarios = [
 ]
 
 function VerComentarios() {
-
     for(let contador = 0; contador < comentarios.length;contador++ ) {
         console.log(comentarios[contador])
     }
-
 }
 
 function BuscarComentario(indice) {
     for(let contador = 0; contador < comentarios.length;contador++ ) {
-        //muestro  cada uno de los comentarios
-        if(contador === indice) { //si es igual al  indice  "0" se ejecuta el codigo
-                console.log(comentarios[contador])
+    //muestro  cada uno de los comentarios
+        if(contador  === indice) { //si es igual al  indice  "0" se ejecuta el codigo
+            console.log("Este es el comentario que busca", comentarios[contador])
         }
     }
 }
 
+function eliminarComentario(indice ) {
+    comentarios.splice(indice,1)
+    console.log(comentarios)
+}
 
-export {comentarios, VerComentarios, BuscarComentario}
+function actualizarComentario(indice, nuevoComentario) {
+    comentarios[indice] = nuevoComentario
+    console.log(comentarios)
+}
+
+
+
+export {comentarios, VerComentarios, actualizarComentario, BuscarComentario, eliminarComentario}
